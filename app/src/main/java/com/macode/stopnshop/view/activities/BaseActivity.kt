06170df339ae6c791792c439f0380f2875984ee7
Marchael Acode <m.acode@outlook.com>
2021-05-02 23:47:20 -0700

    The user is able to click on the setup image view to take or attach a profile image. Once clicked, permissions are asked for taking a picture or selecting from internal files.

--- app/src/main/java/com/macode/stopnshop/view/activities/BaseActivity.kt
+++ app/src/main/java/com/macode/stopnshop/view/activities/BaseActivity.kt
@@ -1,26 +1,51 @@
 package com.macode.stopnshop.view.activities
 
+import android.Manifest
+import android.app.Activity
+import android.app.AlertDialog
 import android.app.Dialog
+import android.content.ActivityNotFoundException
+import android.content.Context
+import android.content.ContextWrapper
+import android.content.Intent
+import android.graphics.Bitmap
 import android.net.Uri
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.provider.Settings
 import android.widget.TextView
 import androidx.core.content.ContextCompat
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textfield.TextInputLayout
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionDeniedResponse
+import com.karumi.dexter.listener.PermissionGrantedResponse
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
+import com.karumi.dexter.listener.single.PermissionListener
 import com.macode.stopnshop.R
+import com.macode.stopnshop.databinding.ImageSelectionDialogBinding
 import com.macode.stopnshop.firebase.FireStoreClass
 import com.macode.stopnshop.model.User
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+import java.io.OutputStream
 import java.text.SimpleDateFormat
 import java.util.*
 
 open class BaseActivity : AppCompatActivity() {
 
     companion object {
-
+        const val CAMERA = 1
+        const val GALLERY = 2
+        const val IMAGE_DIRECTORY = "StopNShopImages"
     }
 
     var userDetails: User = User()
@@ -33,6 +58,101 @@ open class BaseActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_base)
+    }
+
+    fun imageSelectionDialog(activity: Activity) {
+        val dialog = Dialog(activity)
+        val dialogBinding: ImageSelectionDialogBinding = ImageSelectionDialogBinding.inflate(layoutInflater)
+        dialog.setContentView(dialogBinding.root)
+
+        dialogBinding.cameraSelection.setOnClickListener {
+            Dexter.withContext(activity).withPermissions(
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.CAMERA
+            ).withListener(object: MultiplePermissionsListener {
+                override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
+                    report?.let {
+                        if (report.areAllPermissionsGranted()) {
+                            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                            startActivityForResult(intent, CAMERA)
+                            dialog.dismiss()
+                        } else {
+                            showRationalDialogForPermissions()
+                        }
+                    }
+                }
+
+                override fun onPermissionRationaleShouldBeShown(
+                    permission: MutableList<PermissionRequest>?,
+                    token: PermissionToken?
+                ) {
+                    showRationalDialogForPermissions()
+                }
+
+            }).onSameThread().check()
+        }
+
+        dialogBinding.gallerySelection.setOnClickListener {
+            Dexter.withContext(activity).withPermission(
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            ).withListener(object: PermissionListener {
+                override fun onPermissionGranted(permission: PermissionGrantedResponse?) {
+                    val galleryIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                    startActivityForResult(galleryIntent, GALLERY)
+                    dialog.dismiss()
+                }
+
+                override fun onPermissionDenied(permission: PermissionDeniedResponse?) {
+                    showErrorSnackBar("You have denied the storage permission to select image from gallery!", true)
+                }
+
+                override fun onPermissionRationaleShouldBeShown(
+                    permission: PermissionRequest?,
+                    token: PermissionToken?
+                ) {
+                    showRationalDialogForPermissions()
+                }
+
+            }).onSameThread().check()
+        }
+
+        dialog.show()
+    }
+
+    fun showRationalDialogForPermissions() {
+        AlertDialog.Builder(this).setMessage("It looks like you have turned off permission required for this feature. " +
+                "It can be enabled under Application Settings.")
+            .setPositiveButton("Go to settings")
+            {_, _ ->
+                try {
+                    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                    val uri = Uri.fromParts("package", packageName, null)
+                    intent.data = uri
+                    startActivity(intent)
+                } catch (e: ActivityNotFoundException) {
+                    e.printStackTrace()
+                }
+            }.setNegativeButton("Cancel")
+            {dialog, _ ->
+                dialog.dismiss()
+            }.show()
+    }
+
+    fun convertToImageFile(bitmap: Bitmap): Uri {
+        val wrapper = ContextWrapper(applicationContext)
+        var file = wrapper.getDir(IMAGE_DIRECTORY, Context.MODE_PRIVATE)
+        file = File(file, "${UUID.randomUUID()}.png")
+
+        try {
+            val stream: OutputStream = FileOutputStream(file)
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream)
+            stream.flush()
+            stream.close()
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+
+        return Uri.fromFile(file.absoluteFile)
     }
 
     fun getDate(): String {
--- app/src/main/java/com/macode/stopnshop/view/activities/SetUpActivity.kt
+++ app/src/main/java/com/macode/stopnshop/view/activities/SetUpActivity.kt
@@ -1,15 +1,29 @@
 package com.macode.stopnshop.view.activities
 
+import android.Manifest
+import android.app.Activity
+import android.app.Dialog
 import android.content.Intent
+import android.graphics.Bitmap
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.View
 import androidx.appcompat.widget.Toolbar
 import com.bumptech.glide.Glide
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionDeniedResponse
+import com.karumi.dexter.listener.PermissionGrantedResponse
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
+import com.karumi.dexter.listener.single.PermissionListener
 import com.macode.stopnshop.R
 import com.macode.stopnshop.databinding.ActivitySetUpBinding
+import com.macode.stopnshop.databinding.ImageSelectionDialogBinding
 import com.macode.stopnshop.model.User
 import com.macode.stopnshop.utilities.Constants
 import com.macode.stopnshop.utilities.SNSTextView
@@ -30,6 +44,27 @@ class SetUpActivity : BaseActivity(), View.OnClickListener {
         setUpUserDetails(userDetails)
 
         setUpToolbar()
+
+        binding!!.setUpProfileImage.setOnClickListener(this)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == CAMERA) {
+                data?.extras?.let {
+                    val bitmap: Bitmap = data.extras!!.get("data") as Bitmap
+                    Glide
+                        .with(this@SetUpActivity)
+                        .load(bitmap)
+                        .centerCrop()
+                        .placeholder(R.drawable.profile_place_holder)
+                        .into(binding!!.setUpProfileImage)
+
+                    selectedImageUri = convertToImageFile(bitmap)
+                }
+            }
+        }
     }
 
     private fun setUpToolbar() {
@@ -61,12 +96,10 @@ class SetUpActivity : BaseActivity(), View.OnClickListener {
     override fun onClick(v: View?) {
         when (v!!.id) {
             R.id.setUpProfileImage -> {
-                imageSelectionDialog()
+                imageSelectionDialog(this@SetUpActivity)
             }
         }
     }
 
-    private fun imageSelectionDialog() {
 
-    }
 }
